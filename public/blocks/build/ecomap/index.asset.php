2c53037eff177afb1429b3f75aa54ae499444856
Anton Lukin <anton@lukin.me>
2024-07-03 11:42:32 +0200

    add title limit gutenberg plugin

--- public/blocks/build/ecomap/index.asset.php
+++ public/blocks/build/ecomap/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ecb8d1fe2c9597e64a3c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fa3ee6b22865cd419291');
